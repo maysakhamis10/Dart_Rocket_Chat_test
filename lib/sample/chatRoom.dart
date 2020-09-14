24c6d0ce0dfd88cacb3c979c86a678f186b3232f
FatmaMM <fabdelmuhsen@egypt.itgsolutions.com>
2020-09-14 10:56:32 +0200

    scroll to last message in listView

--- lib/sample/chatRoom.dart
+++ lib/sample/chatRoom.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:jitsi/models/models.dart';
 import 'package:jitsi/realtime/client.dart';
+import 'package:jitsi/resourses/Styles.dart';
 import 'package:jitsi/rest/client.dart';
 import 'package:jitsi/ui/chat_room/CustomMessageInput.dart';
 import 'package:jitsi/ui/chat_room/CustomMessageText.dart';
@@ -16,7 +19,7 @@ class ChatRoom extends StatefulWidget {
   _ChatRoomState createState() => _ChatRoomState();
 }
 
-class _ChatRoomState extends State<ChatRoom> {
+class _ChatRoomState extends State<ChatRoom> with WidgetsBindingObserver {
   TextEditingController _text = new TextEditingController();
   var childList = <Widget>[];
   Client client = new Client(
@@ -30,19 +33,17 @@ class _ChatRoomState extends State<ChatRoom> {
   UserCredentials userCredentials = new UserCredentials(
       id: "b5xdRX58zNb2DmqzA",
       token: "nTNC-NpoY2KChS184hQWPjtdvW_J6AzmsafJBJGBRGk");
-  ScrollController _scrollController = new ScrollController();
+  ScrollController _scrollController;
+  StreamController<bool> streamController = StreamController<bool>();
 
   @override
   void initState() {
     super.initState();
     clientReal = widget.clientReal;
     client.setCredentials(userCredentials);
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _scrollController.animateTo(
-          _scrollController.position.maxScrollExtent,
-          duration: const Duration(milliseconds: 200),
-          curve: Curves.easeOut);
-    });
+    _scrollController = new ScrollController();
+    WidgetsBinding.instance.addObserver(this);
+
 //    Future<List<Channel>> channels = widget.clientReal.getChannelsIn();
 //    Stream<Channel> streamChannel=widget.clientReal.roomsChanged();
 //    streamChannel.listen((onData){
@@ -55,10 +56,23 @@ class _ChatRoomState extends State<ChatRoom> {
 /*
     widget.clientReal.subRoomsChanged(widget.id);
 */
+    streamController.stream.listen((event) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+            duration: const Duration(milliseconds: 200), curve: Curves.easeOut);
+      });
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addObserver(this);
     return Scaffold(
         appBar: AppBar(
           title: FutureBuilder<ChannelSubscription>(
@@ -72,54 +86,82 @@ class _ChatRoomState extends State<ChatRoom> {
         body: Container(
             child: Stack(fit: StackFit.loose, children: <Widget>[
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Flexible(
-                fit: FlexFit.tight,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Flexible(
+                  fit: FlexFit.tight,
                   child: Container(
-                      child: FutureBuilder<List<Message>>(
-                          future: client.loadIMHistory(widget.roomId),
-                          builder: (context, snapshot) {
-
-                            return snapshot.hasData
-                                ? ListView.builder(
-                                    controller: _scrollController,
-                                    itemCount: snapshot.data.length,
-                                    itemBuilder: (_, int position) {
-                                      final item = snapshot.data[position];
-                                      return MessageItem(
-                                        message: item.msg,
-                                        time: item.timestamp,
-                                        messageType:
-                                            userCredentials.id == item.user.id
-                                                ? MessageType.sent
-                                                : MessageType.received,
-                                      );
-                                    })
-                                : Center(
-                                    child: CircularProgressIndicator(),
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                        child: FutureBuilder<List<Message>>(
+                            future: client.loadIMHistory(widget.roomId),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData && snapshot.data != null) {
+                                if (snapshot.data.length == 0) {
+                                  return Center(
+                                    child: Text(
+                                      "Start messaging...",
+                                      style: MESSAGE_TEXT_STYLE,
+                                    ),
                                   );
-                          })),
+                                } else {
+                                  return ListView.builder(
+                                      controller: _scrollController,
+                                      itemCount: snapshot.data.length,
+                                      itemBuilder: (_, int position) {
+                                        final item = snapshot.data[position];
+                                        streamController.add(true);
+                                        return MessageItem(
+                                          message: item.msg,
+                                          time: item.timestamp,
+                                          messageType:
+                                              userCredentials.id == item.user.id
+                                                  ? MessageType.sent
+                                                  : MessageType.received,
+                                        );
+                                      });
+                                }
+                              } else
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                            })),
+                  ),
                 ),
-              ),
-              Divider(height: 0, color: Colors.black26),
-              Container(
-                color: Colors.white,
-                height: 50,
-                child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: CustomMessageInput(
-                      sendMessage: sendMessage,
-                    )),
-              )
-            ],
-          )
+                Divider(height: 0, color: Colors.black26),
+                Container(
+                  color: Colors.white,
+                  height: 50,
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: CustomMessageInput(
+                        sendMessage: sendMessage,
+                      )),
+                )
+              ])
         ])));
   }
 
   void sendMessage() {
     clientReal.sendMessage(widget.roomId, _text.text);
+  }
+
+  void didUpdateWidget(ChatRoom oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 200), curve: Curves.easeOut);
+    });
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('state = $state');
+  }
+
+  @override
+  void dispose() {
+    streamController.close();
+    super.dispose();
   }
 }
